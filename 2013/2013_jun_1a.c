7afbe89c256dfeccab0008d373356ed73f30791b
Konstantin Đorđević <vomindoraan@gmail.com>
2017-06-05 23:02:46 +0200

    Fix 2013 jun 1a

--- 2013/2013_jun_1a.c
+++ 2013/2013_jun_1a.c
@@ -11,14 +11,14 @@ float razdaljina(Tacka t1, Tacka t2)
     return sqrt(pow(t1.x-t2.x, 2) + pow(t1.y-t2.y, 2) + pow(t1.z-t2.z, 2));
 }
 
-float nadji_tacke(int n, Tacka niz[], int *idx1, int *idx2)
+float nadji_tacke(Tacka niz[], int n, int *idx1, int *idx2)
 {
     float razd = 0;
     int i, j;
 
     for (i = 0; i < n-1; ++i) {
         for (j = i+1; j < n; ++j) {
-            int nova_razd = razdaljina(niz[i], niz[j]);
+            float nova_razd = razdaljina(niz[i], niz[j]);
             if (nova_razd >= razd) {
                 *idx1 = i;
                 *idx2 = j;
@@ -37,11 +37,11 @@ int main(void)
     float max_razd;
     
     scanf("%d", &n);
-    if (n < 1 || n > MAX) {
+    if (n < 2) {
         return 1;
     }
 
-    niz = calloc(n, sizeof(Tacka));
+    niz = calloc(n, sizeof *niz);
     if (!niz) {
         fputs("Neuspesna alokacija memorije", stderr);
         return 2;
@@ -50,7 +50,7 @@ int main(void)
     for (i = 0; i < n; ++i)
         scanf("%f%f%f", &niz[i].x, &niz[i].y, &niz[i].z);
     
-    max_razd = nadji_tacke(n, niz, &idx1, &idx2);
+    max_razd = nadji_tacke(niz, n, &idx1, &idx2);
 
     printf("Maks. razdaljina = %f\nidx1 = %d\nidx2 = %d\n",
            max_razd, idx1, idx2);
