796a8475022367e20c1f6ad7425ab01dbb4854e4
Konstantin Đorđević <vomindoraan@gmail.com>
2017-06-05 11:17:39 +0200

    Update 2014 jun 1a

--- 2014/2014_jun_1a.c
+++ 2014/2014_jun_1a.c
@@ -1,48 +1,51 @@
-#include <stdbool.h>
+#include <stdbool.h> // bool, true, false
 #include <stdio.h>
 #include <stdlib.h>
 
+#define PROV_MEM(p) if (!(p)) puts("Neuspesna alokacija"), exit(1)
+
 bool prost(int n) {
 	int i;
-	for (i = 2; i <= n/2; i++) {
+	if (n <= 1)
+		return false;
+	for (i = 2; i <= n/2; i++)
 		if (n % i == 0)
 			return false;
-	}
 	return true;
 }
 
-void obrada(int **niz, int n){
+void obrada(int **mat, int n) {
     int i, j;
-	for (i = 1; i < n; i++) {
-		for (j = 1; j < n; j++) {
-			if (i + j >= n && prost(niz[i][j])) {
-				niz[i][j] = niz[n-1-j][n-1-i];
-			}
-		}
-	}
+	for (i = 1; i < n; i++)
+		for (j = 1; j < n; j++)
+			if (i + j >= n && prost(mat[i][j]))
+				mat[i][j] = mat[n-1-j][n-1-i];
 }
 
-int main(void){
-	int **niz, n, i, j;
-	printf("Unesite dimenziju matrice: ");
-	scanf("%d", &n);
+int main(int argc, char *argv[]) {
+	int **mat, n, i, j;
 
-	niz= calloc(n, sizeof(int*));
-	for (i=0; i<n; i++){
-		niz[i]=calloc(n, sizeof(int));
-		for(j=0; j<n; j++)
-			scanf("%d", &niz[i][j]);
-	}
+	if (argc != 2)
+		return 2;
+	n = atoi(argv[1]);
 
-	obrada (niz, n);
-	for (i=0; i<n; i++){
-		for(j=0;j<n;j++)
-			printf("%2d ", niz[i][j]);
-		printf("\n");
-	}
+	PROV_MEM(mat = calloc(n, sizeof(int*)));
 
 	for (i = 0; i < n; i++) {
-		free(niz[i]);
+		PROV_MEM(mat[i] = calloc(n, sizeof(int)));
+		for (j = 0; j < n; j++)
+			scanf("%d", &mat[i][j]);
 	}
-	free(niz);
+
+	obrada(mat, n);
+
+	for (i = 0; i < n; i++) {
+		for (j = 0; j < n; j++)
+			printf("%2d ", mat[i][j]);
+		putchar('\n');
+	}
+
+	for (i = 0; i < n; i++)
+		free(mat[i]);
+	free(mat);
 }
