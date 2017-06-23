112ee7536afea0e5695dc65ab0ac77f9c2ffaa5b
Konstantin Đorđević <vomindoraan@gmail.com>
2017-06-23 22:08:16 +0200

    Replace tabs with spaces

--- 2013/2013_jul_1.c
+++ 2013/2013_jul_1.c
@@ -3,48 +3,48 @@
 
 void find_subarray(int *arr, int n, float k, int *pstart, int *plen)
 {
-	for (int len = n; len > 0; len--) {
-		for (int i = 0; i <= n-len; i++) {
-			float sum = 0;
-			
-			for (int j = 0; j < len; j++)
-				sum += arr[i+j];
-			
-			if (sum / len >= k) {
-				*pstart = i;
-				*plen = len;
-				return;
-			}
-		}
-	}
-	*pstart = -1;
-	*plen = 0;
+    for (int len = n; len > 0; len--) {
+        for (int i = 0; i <= n-len; i++) {
+            float sum = 0;
+            
+            for (int j = 0; j < len; j++)
+                sum += arr[i+j];
+            
+            if (sum / len >= k) {
+                *pstart = i;
+                *plen = len;
+                return;
+            }
+        }
+    }
+    *pstart = -1;
+    *plen = 0;
 }
 
 int main(void)
 {
-	puts("Uneti n:");
-	int n;
-	scanf("%d", &n);
+    puts("Uneti n:");
+    int n;
+    scanf("%d", &n);
 
-	int *arr = calloc(n, sizeof *arr);
-	if (!arr) {
-		fputs("Neuspesna alokacija", stderr);
-		return 1;
-	}
+    int *arr = calloc(n, sizeof *arr);
+    if (!arr) {
+        fputs("Neuspesna alokacija", stderr);
+        return 1;
+    }
 
-	puts("Uneti niz:");
-	for (int i = 0; i < n; i++)
-		scanf("%d", arr+i);
+    puts("Uneti niz:");
+    for (int i = 0; i < n; i++)
+        scanf("%d", arr+i);
 
-	puts("Uneti k:");
-	float k;
-	scanf("%f", &k);
+    puts("Uneti k:");
+    float k;
+    scanf("%f", &k);
 
-	int start, len;
-	find_subarray(arr, n, k, &start, &len);
+    int start, len;
+    find_subarray(arr, n, k, &start, &len);
 
-	puts("Najduzi podniz je:");
-	for (int i = 0; i < len; i++)
-		printf("%d ", arr[start+i]);
+    puts("Najduzi podniz je:");
+    for (int i = 0; i < len; i++)
+        printf("%d ", arr[start+i]);
 }
--- 2014/2014_jul_2.c
+++ 2014/2014_jul_2.c
@@ -15,90 +15,90 @@
 typedef char word_t[WORD_LEN+1];
 
 typedef struct node {
-	word_t word;
-	int    n;
-	struct node *next;
+    word_t word;
+    int    n;
+    struct node *next;
 } Node;
 
 Node *find_word(Node *list, word_t word)
 {
-	for (Node *p = list; p; p = p->next)
-		if (strcmp(p->word, word) == 0)
-			return p;
-	return NULL;
+    for (Node *p = list; p; p = p->next)
+        if (strcmp(p->word, word) == 0)
+            return p;
+    return NULL;
 }
 
 Node *read_wordlist(FILE *fp)
 {
-	Node *head = NULL, *tail;
-	word_t word;
-	char c, i = 0;
+    Node *head = NULL, *tail;
+    word_t word;
+    char c, i = 0;
 
-	while ((c = fgetc(fp)) != EOF) {
-		if (isalpha(c)) {
-			word[i++] = tolower(c); // Pamti kao sva mala slova
-			continue;
-		} else if (i == 0)
-			continue; // Preskace uzastopne razmake/interpunkciju
+    while ((c = fgetc(fp)) != EOF) {
+        if (isalpha(c)) {
+            word[i++] = tolower(c); // Pamti kao sva mala slova
+            continue;
+        } else if (i == 0)
+            continue; // Preskace uzastopne razmake/interpunkciju
 
-		word[i] = '\0';
-		i = 0;
+        word[i] = '\0';
+        i = 0;
 
-		Node *node = find_word(head, word);
-		if (node) {
-			node->n++;
-			continue;
-		}
+        Node *node = find_word(head, word);
+        if (node) {
+            node->n++;
+            continue;
+        }
 
-		ALLOC_CHECK(node = malloc(sizeof *node));
-		strcpy(node->word, word);
-		node->n = 1;
-		node->next = NULL;
+        ALLOC_CHECK(node = malloc(sizeof *node));
+        strcpy(node->word, word);
+        node->n = 1;
+        node->next = NULL;
 
-		if (head) tail->next = node;
-		else      head = node;
-		tail = node;
-	}
+        if (head) tail->next = node;
+        else      head = node;
+        tail = node;
+    }
 
-	return head;
+    return head;
 }
 
 void compare(Node *list1, Node *list2)
 {
-	int n1 = 0, n2 = 0, n12 = 0;
+    int n1 = 0, n2 = 0, n12 = 0;
 
-	for (Node *p1 = list1; p1; p1 = p1->next) {
-		Node *p2 = find_word(list2, p1->word);
-		if (!p2) {
-			n1 += p1->n;
-			continue;
-		}
+    for (Node *p1 = list1; p1; p1 = p1->next) {
+        Node *p2 = find_word(list2, p1->word);
+        if (!p2) {
+            n1 += p1->n;
+            continue;
+        }
 
-		int dn = p1->n - p2->n;
-		n1  += max(dn,  0);
-		n2  += max(-dn, 0);
-		n12 += min(p1->n, p2->n);
+        int dn = p1->n - p2->n;
+        n1  += max(dn,  0);
+        n2  += max(-dn, 0);
+        n12 += min(p1->n, p2->n);
 
-		p2->n = 0; // Sprecava ponovno dodavanje u drugoj petlji
-	}
+        p2->n = 0; // Sprecava ponovno dodavanje u drugoj petlji
+    }
 
-	for (Node *p2 = list2; p2; p2 = p2->next)
-		n2 += p2->n;
+    for (Node *p2 = list2; p2; p2 = p2->next)
+        n2 += p2->n;
 
-	printf("isto - %d\noriginal - %d\nkopija - %d\n", n12, n1, n2);
+    printf("isto - %d\noriginal - %d\nkopija - %d\n", n12, n1, n2);
 }
 
 int main(void)
 {
-	FILE *f_orig = fopen("original.txt", "r"),
-	     *f_copy = fopen("kopija.txt",   "r");
-	FILE_CHECK(f_orig && f_copy);
+    FILE *f_orig = fopen("original.txt", "r"),
+         *f_copy = fopen("kopija.txt",   "r");
+    FILE_CHECK(f_orig && f_copy);
 
-	Node *list_orig = read_wordlist(f_orig),
-	     *list_copy = read_wordlist(f_copy);
+    Node *list_orig = read_wordlist(f_orig),
+         *list_copy = read_wordlist(f_copy);
 
-	compare(list_orig, list_copy);
+    compare(list_orig, list_copy);
 
-	fclose(f_orig), fclose(f_copy);
-	free(list_orig), free(list_copy);
+    fclose(f_orig), fclose(f_copy);
+    free(list_orig), free(list_copy);
 }
