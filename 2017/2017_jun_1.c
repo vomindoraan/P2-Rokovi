fdec94e49720be835a73db4b21e8eb042fe1f74c
Konstantin Đorđević <vomindoraan@gmail.com>
2017-06-27 01:44:48 +0200

    Add 2017 jun 1

---
+++ 2017/2017_jun_1.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define ALLOC_CHECK(p) if (!(p)) printf("Neuspesna alokacija\n"), exit(1)
+
+char **read(int m, int n)
+{
+    char **mat = malloc(m * sizeof(*mat));
+    ALLOC_CHECK(mat);
+
+    for (int i = 0; i < m; i++) {
+        mat[i] = malloc(n * sizeof(**mat));
+        ALLOC_CHECK(mat[i]);
+
+        for (int j = 0; j < n; j++)
+            do {
+                mat[i][j] = getchar();
+            } while (!strchr("KDTLSPO", mat[i][j]));
+    }
+
+    return mat;
+}
+
+void solve(char **mat1, char **mat2, int m, int n)
+{
+    int di[] = { -1, -2, -2, -1, +1, +2, +2, +1 },
+        dj[] = { -2, -1, +1, +2, +2, +1, -1, -2 };
+
+    for (int i = 0; i < m; i++)
+        for (int j = 0; j < n; j++)
+            if (mat1[i][j] == 'S') {
+                printf("\nSkakac [%d,%d] moze da pojede:\n", i, j);
+
+                for (int k = 0; k < 8; k++) {
+                    int x = i + di[k], y = j + dj[k];
+                    if (x >= 0 && x < m && y >= 0 && y < n && mat2[x][y] != 'O')
+                        printf("[%d,%d] %c\n", x, y, mat2[x][y]);
+                }
+            }
+}
+
+void delete(char **mat, int m)
+{
+    for (int i = 0; i < m; i++)
+        free(mat[i]);
+    free(mat);
+}
+
+int main(void)
+{
+    printf("Uneti velicinu table: ");
+    int m, n;
+    scanf("%d%d", &m, &n);
+
+    printf("Uneti bele figure:\n");
+    char **white = read(m, n);
+
+    printf("Uneti crne figure:\n");
+    char **black = read(m, n);
+
+    printf("Uneti boju [b/c]: ");
+    switch (getchar(), getchar()) { // Guta '\n' viska
+    case 'b': solve(white, black, m, n); break;
+    case 'c': solve(black, white, m, n); break;
+    }
+
+    delete(white, m);
+    delete(black, m);
+}
