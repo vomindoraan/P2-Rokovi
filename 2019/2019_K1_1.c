4407bf33832dba524265ed7fd433f860980aa4d3
Konstantin Đorđević <vomindoraan@gmail.com>
2019-10-07 21:23:58 +0200

    Use an array for storing max values in 2019 K1 1

--- 2019/2019_K1_1.c
+++ 2019/2019_K1_1.c
@@ -36,24 +36,27 @@ int main(void)
         }
         putchar('\n');
 
-        unsigned max0 = 0, max1 = 0, max2 = 0;
+        unsigned max[] = {0, 0, 0};
         for (int i = 0; i < n; i++) {
-            if (array[i] > max0) {
-                max2 = max1;
-                max1 = max0;
-                max0 = array[i];
-            } else if (array[i] > max1) {
-                max2 = max1;
-                max1 = array[i];
-            } else if (array[i] > max2) {
-                max2 = array[i];
+            if (array[i] > max[0]) {
+                max[2] = max[1];
+                max[1] = max[0];
+                max[0] = array[i];
+            } else if (array[i] > max[1]) {
+                max[2] = max[1];
+                max[1] = array[i];
+            } else if (array[i] > max[2]) {
+                max[2] = array[i];
             }
         }
 
-        if (n >= 1) printf("%u\n", max0);
-        if (n >= 2) printf("%u\n", max1);
-        if (n >= 3) printf("%u\n", max2);
-        for (int i = 0; i < 3-n; i++) puts("x");
+        for (int i = 0; i < 3; i++) {
+            if (n > i) {
+                printf("%u\n", max[i]);
+            } else {
+                puts("x");
+            }
+        }
     }
 
     return 0;
